20a97b00d6a56c54d37fb2798890c6fdbba37ba3
vampire-ab <brijeshagal@gmail.com>
2022-10-27 15:03:12 +0530

    Deploying

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="items-center justify-between flex fixed w-full flex-col md:flex-row bg-white shadow-gray-400 shadow-md h-[100px] z-10">
+    <div className="items-center justify-between flex fixed w-full flex-col md:flex-row bg-white shadow-gray-400 shadow-md h-[100px] z-[1000]">
       <nav className=" rounded-xl m-2 bg-white">
         <header className="bg-clip-text text-2xl md:text-4xl text-center font-bold text-transparent bg-gradient-to-r from-blue-900 to-blue-400 md:m-3 m-2">AKSH INVESTMENT</header>
         <h2 className='md:m-2 md:font-semibold text-right m-2'>DISCOVER THE NEW STYLE OF INVESTING</h2>
