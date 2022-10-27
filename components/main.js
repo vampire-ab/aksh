6eda4bf358afc1f06dd9b64a0f2dd87e4eb7214a
vampire-ab <brijeshagal@gmail.com>
2022-10-28 01:05:23 +0530

    Our Services Section

--- components/main.js
+++ components/main.js
@@ -4,12 +4,14 @@ import Statistics from './/svg/statistics-bro.svg'
 // import BullBear from "/BullBear1.png";
 const Main = () => {
   return (
-    <div className='relative top-[100px] h-[600px] justify-center flex items-center flex-wrap'>
+    <div className='relative top-[100px] justify-center flex items-center flex-wrap'>
       <Statistics className="text-black h-full" />
       <div className="">
         {/* <h1>What do we do?</h1> */}
-        <h2>TECHNICAL ANALYSIS</h2>
-        
+        <h2 className='text-4xl'>TECHNICAL ANALYSIS</h2>
+        <div>
+          <p></p>
+        </div>
       </div>
     </div>
   )
--- components/services.js
+++ components/services.js
@@ -1,24 +1,32 @@
 import React from 'react'
-
+import MoneyIncome from ".//svg/moneyIncome.svg"
 
 const Services = () => {
   return (
     <div className='top-[120px] relative p-12 bg-blue-400 '>
+      <style jsx>{`
+        .linearGradient{
+          background: linear-gradient(-180deg, #60A5FA 20%, rgba(255, 255, 255, 100) 10%));
+        }
+      `}</style>
       <header className="font-semibold text-2xl text-white">OUR SERVICES</header>
-      <div className='m-8'>
-        <div className="max-w-sm rounded-md overflow-hidden shadow-md bg-white shadow-white hover:-translate-y-1 ease-in-out">
+      <div className='md:m-8 pt-5 flex flex-wrap justify-center items-center'>
+        <div className="max-w-sm rounded-md overflow-hidden hover:shadow-md bg-white shadow-gray-700 hover:-translate-y-1 linearGradient">
           {/* <img className="w-full object-cover" src = "/MutualFunds.jpeg" alt="Mutual Funds" /> */}
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">Mutual Funds</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Hashtags if any</span>
-              {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+
+          <div className="px-1 md:px-6 pb-4 flex flex-col justify-center">
+            <div className='flex justify-center'>
+              <MoneyIncome /></div>
+            <div className="font-bold text-xl mb-2">Mutual Funds</div>
+            <p className="text-gray-700 text-base ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Hashtags if any</span>
+            {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span> */}
-            </div>
+          </div>
         </div>
       </div>
     </div>
